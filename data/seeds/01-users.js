748e16003855de2ef4a9564b5304820b30d69b65
benjamin-solt <bensolt@gmail.com>
2021-04-19 12:57:15 -0400

    adding id

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -5,23 +5,27 @@ exports.seed = function(knex, Promise) {
       .truncate()
       .then(function() {
         return knex('halo').insert([
-          { 
+          { id: 1,
             name: 'master chief',
             ingredients: 'ingreds 1'
         },
           { 
+            id: 2,
             name: 'cortana',
             ingredients: 'ingreds 2'
         },
           { 
+            id: 3,
             name: 'arbiter',
             ingredients: 'ingreds 3'
          },
           { 
+            id: 4,
             name: 'captain keys',
             ingredients: 'ingreds 4'
          },
           { 
+            id: 5,
             name: 'dr halsey',
             ingredients: 'ingreds 5' }
         ]);
