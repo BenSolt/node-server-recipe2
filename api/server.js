cd3862e1a06ff177ad60ce63bf2cb6bcac6182f3
benjamin-solt <bensolt@gmail.com>
2021-05-27 14:13:25 -0400

    adding ocr capability

--- api/server.js
+++ api/server.js
@@ -16,7 +16,7 @@ server.get("/", (req, res) => {
 
 //GET CHARACTERS
 server.get("/char", (req, res) => {
-    Halo.find()
+  Halo.find()
     .then(halo => {
       res.status(200).json(halo);
     })
@@ -27,9 +27,9 @@ server.get("/char", (req, res) => {
 
 //GET CHARACTERS BY ID
 server.get('/char/:id', (req, res) => {
-    const { id } = req.params;
-  
-    Halo.findById(id)
+  const { id } = req.params;
+
+  Halo.findById(id)
     .then(char => {
       if (char) {
         res.json(char);
@@ -40,13 +40,13 @@ server.get('/char/:id', (req, res) => {
     .catch(err => {
       res.status(500).json({ err, message: 'Failed to get recipe' });
     });
-  });
+});
 
 //POST (CREATE)
 server.post('/char', (req, res) => {
-    const data = req.body;
-  
-    Halo.add(data)
+  const data = req.body;
+
+  Halo.add(data)
     .then(char => {
       // Halo.find()
       //   .then(halo => {
@@ -54,62 +54,92 @@ server.post('/char', (req, res) => {
       //   })
       res.status(201).json(char);
     })
-    .catch (err => {
+    .catch(err => {
       res.status(500).json({ message: 'Failed to create new recipe' });
     });
-  });
+});
 
 //POST (CREATE) OCR ////////////////////////////////////////////
 
-const storage=multer.diskStorage({
-  destination:(req,file,cb)=>{
-      cb(null,'./uploads/');
-  },
-  filename:(req,file,cb)=>{
-      cb(null,file.originalname);
+app.get("/uploads/:id", (req, res) => {
+  console.log(__dirname)
+  res.send(fs.readFileSync(`${__dirname}\\uploads\\${req.params.id}`));
+});
+
+
+
+const fileUpload = require('express-fileupload');
+app.use(fileUpload({ debug: true }));
+
+// Upload Endpoint
+app.post("/upload", (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
   }
-})
 
-const upload= multer({storage:storage});
+  const file = req.files.file;
 
-server.post('/char/upload', upload.single('uploadedImage'),(req,res)=>{
-  console.log(req.file);
-
-  try{
-      Tesseract.recognize(
-          'uploads/' +req.file.filename,
-          'eng',
-          {logger:m =>console.log(m)}
-        
-      )
-      .then(({data:{text}})=>{
-
-          return res.json(
-              {
-                  message:text
-              }
-          )
-      })
-
-  }catch(error){
-      console.error(error, 'error1')
-  }
-})
-
-// UPDATE (EDIT)  ? is this what I need to add?
-
-server.put('/char/:id', (req, res) =>{
-  Halo.update(req.body, req.params.id)
-  .then(char =>{
-    if (char) {
-      res.json(char)
-    } else {
-      res.status(404).json({message: "Recipe with specified ID does not exist"})
+  file.mv(`${__dirname}\\uploads\\${file.name}`, err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
     }
-  })
-  .catch(err =>{
-    res.status(500).json({message: "Could not update Recipe"})
-  })
+
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
+
+
+// const storage=multer.diskStorage({
+//   destination:(req,file,cb)=>{
+//       cb(null,'./uploads/');
+//   },
+//   filename:(req,file,cb)=>{
+//       cb(null,file.originalname);
+//   }
+// })
+
+// const upload= multer({storage:storage});
+
+// app.post('/upload', (req, res) => {
+//   upload(req, res, err => {
+//       console.log(req.file);
+//       fs.readFile(`./uploads/${req.file.originalname}`, (err, data) => {
+//           if (err) return console.log('error');
+//           try {
+//               Tesseract
+//                   .recognize(
+//                       'uploads/' + req.file.filename,
+//                       'eng',
+//                   { logger: m => console.log(m) }
+//                   )
+//                   .then(({ data: { text } }) => {
+//                       return res.json(
+//                           {
+//                               text
+//                           }
+//                       )
+//                   })
+//           } catch (error) {
+//               console.error(error)
+//           }
+//       });
+//   });
+// });
+
+// EDIT RECIPE
+server.put('/char/:id', (req, res) => {
+  Halo.update(req.body, req.params.id)
+    .then(char => {
+      if (char) {
+        res.json(char)
+      } else {
+        res.status(404).json({ message: "Recipe with specified ID does not exist" })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Could not update Recipe" })
+    })
 })
 
 
@@ -117,16 +147,16 @@ server.delete('/char/:id', (req, res) => {
   const { id } = req.params;
 
   Halo.remove(id)
-  .then(deleted => {
-    if (deleted) {
-      res.json({ removed: deleted });
-    } else {
-      res.status(404).json({ message: 'Could not find character with given id' });
-    }
-  })
-  .catch(err => {
-    res.status(500).json({ message: 'Failed to delete character' });
-  });
+    .then(deleted => {
+      if (deleted) {
+        res.json({ removed: deleted });
+      } else {
+        res.status(404).json({ message: 'Could not find character with given id' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to delete character' });
+    });
 });
 
 
