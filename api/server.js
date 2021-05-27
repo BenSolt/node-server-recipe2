41e5754d1164592ec47500433ad188a56bf3e44f
benjamin-solt <bensolt@gmail.com>
2021-05-27 15:52:19 -0400

    tes3

--- api/server.js
+++ api/server.js
@@ -72,22 +72,22 @@ server.post('/char', (req, res) => {
 // app.use(fileUpload({ debug: true }));
 
 // // Upload Endpoint
-// server.post("/upload", (req, res) => {
-//   if (req.files === null) {
-//     return res.status(400).json({ msg: 'No file uploaded' });
-//   }
+server.post("/upload", (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
+  }
 
-//   const file = req.files.file;
+  const file = req.files.file;
 
-//   file.mv(`${__dirname}\\uploads\\${file.name}`, err => {
-//     if (err) {
-//       console.error(err);
-//       return res.status(500).send(err);
-//     }
+  file.mv(`${__dirname}\\uploads\\${file.name}`, err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
 
-//     res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
-//   });
-// });
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
 
 ////////////////////////////////////////////////////////////////////
 //NOT USING BELOW
